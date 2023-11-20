d54db9c4425f0384cf79bbba68c83d3385f92b15
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-19 22:12:44 -0300

    feat: add Player class

---
+++ TP_POO/src/estadisticas/Team.java
@@ -0,0 +1,29 @@
+package estadisticas;
+
+import  java.util.ArrayList;
+public class Team {
+    private String name;
+    private ArrayList<Player> players;
+    private int tableScore;
+    private int goalCounter;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void addPlayer(Player player) {
+
+    }
+    public void removePlayer (Player player){
+
+    }
+}
