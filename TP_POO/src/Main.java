f1bdea042ad3b854335b98cf615ee7ac1976f2c7
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 20:55:12 -0300

    feat: remove players

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -5,10 +5,7 @@ import estadisticas.Team;
 public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
-        
-
-        System.out.println(pulga.getTeam().getName());
-        
+  
         argentina.showPlayersList();
     }
 }
