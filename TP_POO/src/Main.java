e181016f76e9786e6a1fad06896c1a579a28c146
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 19:43:46 -0300

    Feature/sm/assig player on team (#1)
    * feat: add new player to team list
    
    * refactor: update player names
    
    * refactor: update addToTeamList method
    
    * feat: add player to team list on create
    
    * feat: add new player
    
    * feat; add new player

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
 
-        Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
-        Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
+
+        Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
+        Player pulga = new Player("Lionel Messi", 10, "Delantero", argentina);
         Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
+        Player taglia = new Player("Nicolas Tagliafico", 3, "Defensor", argentina);
+        Player dePaul = new Player("Rodrigo De Paul", 8, "Medio Campista", argentina);
 
-        messi.getTeam().getPlayers().add(messi);
-        dimaria.getTeam().getPlayers().add(dimaria);
-        dibu.getTeam().getPlayers().add(dibu);
-
-
+        System.out.println(pulga.getTeam().getName());
+        
         argentina.showPlayersList();
     }
 }
