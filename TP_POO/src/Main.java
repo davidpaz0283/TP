ffc7c45e03ee2ca945564b89984c8c9d3bf45ad1
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 20:57:06 -0300

    feat: show player list of players

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -5,7 +5,16 @@ import estadisticas.Team;
 public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
-  
+
+
+        Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
+        Player pulga = new Player("Lionel Messi", 10, "Delantero", argentina);
+        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
+        Player taglia = new Player("Nicolas Tagliafico", 3, "Defensor", argentina);
+        Player dePaul = new Player("Rodrigo De Paul", 8, "Medio Campista", argentina);
+
+        System.out.println(pulga.getTeam().getName());
+
         argentina.showPlayersList();
     }
 }
