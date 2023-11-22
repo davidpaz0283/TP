b5585919d3ec762298fc83e80b0a0a5d72cf2274
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 20:51:07 -0300

    feat: remove player

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -6,16 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
 
-        Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
-        Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
-        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
-        Player dePaul = new Player("Rodrigo De Paul", 8, "Medio Campista", argentina);
-
-        messi.getTeam().getPlayers().add(messi);
-        dimaria.getTeam().getPlayers().add(dimaria);
-        dibu.getTeam().getPlayers().add(dibu);
-        dePaul.getTeam().getPlayers().add(dePaul);
-
 
         argentina.showPlayersList();
     }
