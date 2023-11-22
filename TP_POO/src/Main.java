408ed172fa82c8aeb4e6763661adb1145bd11a7a
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 19:04:43 -0300

    feat: add new player

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
         Player pulga = new Player("Lionel Messi", 10, "Delantero", argentina);
         Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
-
+        Player Taglia = new Player("Nicolas Tagliafico", 3, "Defensor", argentina);
 
         System.out.println(pulga.getTeam().getName());
 
