727ab1fc2e0d0c02065085ccc79da5679d1e1b43
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 23:55:27 -0300

    feat: create francia players

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -4,28 +4,39 @@ import estadisticas.Player;
 import estadisticas.Season;
 import estadisticas.Team;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Main {
+
+    public static String [][] generatePlayer(){
+        String[][] jugadores = new String[2][2];
+
+        for(int i = 0; i < jugadores.length; i++){
+            jugadores[i][0] = JOptionPane.showInputDialog("Por favor ingrese el nombre del jugador que usa la camiseta: " + i+1);
+            jugadores[i][1] = JOptionPane.showInputDialog("Por favor ingrese la posición de: " + jugadores[i][0]);
+        }
+        return jugadores;
+    }
     public static void main(String[] args) {
         Season faseGrupoMundial = new Season();
         Team argentina = new Team("Argentina", 2, 3);
-        Team francia = new Team("Francia", 1, 0);
+        Team francia = new Team("Francia", 0, 0);
 
         faseGrupoMundial.addTeam(argentina);
         faseGrupoMundial.addTeam(francia);
 
-         new Player("Angel Di Maria", 7, "Delantero", argentina);
-         new Player("Lionel Messi", 10, "Delantero", argentina);
-         new Player("Emiliano Martinez", 1, "Arquero", argentina);
+        String[][] jugadoresArgentina = generatePlayer();
+        String[][] jugadoresFrancia = generatePlayer();
 
-        new Player("Kylian Mbappe", 7, "Delantero", francia);
-        new Player("Antoine Griezman", 7, "Delantero", francia);
-        new Player("Hugo Lloris", 1, "Arquero", francia);
+        for(int i = 0; i < jugadoresArgentina.length; i++){
+            new Player(jugadoresArgentina[i][0], i+1, jugadoresArgentina[i][1], argentina);
+        }
+        for(int i = 0; i < jugadoresFrancia.length; i++){
+            new Player(jugadoresFrancia[i][0], i+1, jugadoresFrancia[i][1], francia);
+        }
 
         argentina.showPlayersList();
-
-        francia.showPlayersList();
 
         Match finalMatch = new Match(argentina, francia);
 
