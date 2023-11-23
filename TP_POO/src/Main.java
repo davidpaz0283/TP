3a21dbbc2b0e05b570f4624fdc601a5a59c42c5f
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 21:02:10 -0300

    feat: add player to team

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -8,9 +8,11 @@ public class Main {
 
         Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
         Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
+        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
 
         messi.getTeam().getPlayers().add(messi);
         dimaria.getTeam().getPlayers().add(dimaria);
+        dibu.getTeam().getPlayers().add(dibu);
 
         System.out.println(messi.getTeam().getName());
 
