4aaef154c5736350a876f291cd3255f1bfa64b09
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 21:58:51 -0300

    feat: add new team

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -5,6 +5,7 @@ import estadisticas.Team;
 public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
+        Team francia = new Team("Francia", 1, 0);
 
         Player fideo = new Player("Angel Di Maria", 7, "Delantero", argentina);
         Player pulga = new Player("Lionel Messi", 10, "Delantero", argentina);
@@ -12,8 +13,11 @@ public class Main {
         Player taglia = new Player("Nicolas Tagliafico", 3, "Defensor", argentina);
         Player dePaul = new Player("Rodrigo De Paul", 8, "Medio Campista", argentina);
 
-        System.out.println(pulga.getTeam().getName());
+        Player mbappe = new Player("Kylian Mbappe", 7, "Delantero", francia);
+        Player griezman = new Player("Antoine Griezman", 7, "Delantero", francia);
+        Player lloris = new Player("Hugo Lloris", 1, "Arquero", francia);
 
         argentina.showPlayersList();
+        francia.showPlayersList();
     }
 }
