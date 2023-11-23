3719bab32efcec0c91f21d218d20371569429322
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-22 21:16:53 -0300

    feat: add new players

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -6,6 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Team argentina = new Team("Argentina", 2, 3);
 
-        argentina.showPlayersList();
+        Player dimaria = new Player("Angel Di Maria", 7, "Delantero", argentina);
+        Player messi = new Player("Lionel Messi", 10, "Delantero", argentina);
+        Player dibu = new Player("Emiliano Martinez", 1, "Arquero", argentina);
+
+        messi.getTeam().getPlayers().add(messi);
+        dimaria.getTeam().getPlayers().add(dimaria);
+        dibu.getTeam().getPlayers().add(dibu);
+
+        System.out.println(messi.getTeam().getName());
+
     }
 }
