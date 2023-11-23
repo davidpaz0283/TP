227313753611b1c6ff39f63a7fb2cd5c0697e422
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-23 18:38:29 -0300

    feat: get player info from user input

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -9,45 +9,29 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static String [][] generatePlayer(){
-        String[][] jugadores = new String[2][2];
+    public static String[][] generateArgPlayer() {
+        String[][] jugadoresArgentina = new String[11][2];
 
-        for(int i = 0; i < jugadores.length; i++){
-            jugadores[i][0] = JOptionPane.showInputDialog("Por favor ingrese el nombre del jugador que usa la camiseta: " + i+1);
-            jugadores[i][1] = JOptionPane.showInputDialog("Por favor ingrese la posición de: " + jugadores[i][0]);
+        for (int i = 0; i < jugadoresArgentina.length; i++) {
+            jugadoresArgentina[i][0] = JOptionPane.showInputDialog("Por favor ingrese el nombre del jugador que usa la camiseta: " + (i + 1));
+            jugadoresArgentina[i][1] = JOptionPane.showInputDialog("Por favor ingrese la posición de: " + jugadoresArgentina[i][0]);
         }
-        return jugadores;
+        return jugadoresArgentina;
     }
+
     public static void main(String[] args) {
         Season faseGrupoMundial = new Season();
+        Team argentina = new Team("Argentina", 4, 3);
+        Team francia = new Team("Francia", 0, 0);
 
-        faseGrupoMundial.addTeam(new Team("Argentina"));
-        faseGrupoMundial.addTeam(new Team("Francia"));
+        faseGrupoMundial.addTeam(argentina);
+        faseGrupoMundial.addTeam(francia);
 
-        ArrayList<Team> countries = faseGrupoMundial.getTeams();
+        String[][] jugadoresArgentina = generateArgPlayer();
 
-
-        String[][] jugadoresArgentina = generatePlayer();
-        for(int i = 0; i < jugadoresArgentina.length; i++){
-            new Player(jugadoresArgentina[i][0], i+1, jugadoresArgentina[i][1], countries.get(0));
+        for (int i = 0; i < jugadoresArgentina.length; i++) {
+            new Player(jugadoresArgentina[i][0], i + 1, jugadoresArgentina[i][1], argentina);
         }
-        String[][] jugadoresFrancia = generatePlayer();
-        for(int i = 0; i < jugadoresFrancia.length; i++){
-            new Player(jugadoresFrancia[i][0], i+1, jugadoresFrancia[i][1], countries.get(1));
-        }
-        String[][] jugadoresAlemania = generatePlayer();
-        for(int i = 0; i < jugadoresAlemania.length; i++){
-            new Player(jugadoresAlemania[i][0], i+1, jugadoresAlemania[i][1], countries.get(2));
-        }
-        countries.get(0).showPlayersList();
-
-        Match finalMatch = new Match(countries.get(0), countries.get(1));
-
-        finalMatch.incrementLocalGoals();
-        finalMatch.incrementVisitantGoals();
-        finalMatch.incrementLocalGoals();
-
-
-
+        argentina.showPlayersList();
     }
 }
