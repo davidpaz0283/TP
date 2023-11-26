75483ec307e9174d6aed6a1b62d679f55328be60
davidpaz0283 <151471495+davidpaz0283@users.noreply.github.com>
2023-11-26 15:58:51 -0300

    feat: add cuartos de final

--- TP_POO/src/Main.java
+++ TP_POO/src/Main.java
@@ -16,33 +16,22 @@ public class Main {
         return jugadores;
     }
     public static void main(String[] args) {
-        Llave octavosDeFinalMundial = new Llave(); //
+        Llave cuartosDeFinalMundial = new Llave(); //
 
-        octavosDeFinalMundial.addTeam(new Team("Paises Bajos"));
-        octavosDeFinalMundial.addTeam(new Team("EEUU"));
+        cuartosDeFinalMundial.addTeam(new Team("Paises Bajos"));
+        cuartosDeFinalMundial.addTeam(new Team("Argentina"));
 
-        octavosDeFinalMundial.addTeam(new Team("Argentina"));
-        octavosDeFinalMundial.addTeam(new Team("Australia"));
+        cuartosDeFinalMundial.addTeam(new Team("Inglaterra"));
+        cuartosDeFinalMundial.addTeam(new Team("Francia"));
 
-        octavosDeFinalMundial.addTeam(new Team("Japon"));
-        octavosDeFinalMundial.addTeam(new Team("Croacia"));
+        cuartosDeFinalMundial.addTeam(new Team("Marruecos"));
+        cuartosDeFinalMundial.addTeam(new Team("Portugal"));
 
-        octavosDeFinalMundial.addTeam(new Team("Brasil"));
-        octavosDeFinalMundial.addTeam(new Team("Corea del Sur"));
+        cuartosDeFinalMundial.addTeam(new Team("Brail"));
+        cuartosDeFinalMundial.addTeam(new Team("Croacia"));
 
-        octavosDeFinalMundial.addTeam(new Team("Inglaterra"));
-        octavosDeFinalMundial.addTeam(new Team("Senegal"));
 
-        octavosDeFinalMundial.addTeam(new Team("Francia"));
-        octavosDeFinalMundial.addTeam(new Team("Polonia"));
-
-        octavosDeFinalMundial.addTeam(new Team("Marruecos"));
-        octavosDeFinalMundial.addTeam(new Team("España"));
-
-        octavosDeFinalMundial.addTeam(new Team("Portugal"));
-        octavosDeFinalMundial.addTeam(new Team("Suiza"));
-
-        ArrayList<Team> countries = octavosDeFinalMundial.getTeams();
+        ArrayList<Team> countries = cuartosDeFinalMundial.getTeams();
         for (int j = 0; j < countries.size(); j++) {
             JOptionPane.showMessageDialog(null, "Vamos a cargar los jugadores de : " + countries.get(j).getName());
             String[][] jugadores = generatePlayer();
@@ -51,89 +40,42 @@ public class Main {
             }
         }
 
-        octavosDeFinalMundial.addMatch(new Match(
+        cuartosDeFinalMundial.addMatch(new Match(
                 countries.get(0),
                 countries.get(1))
         );
-        octavosDeFinalMundial.addMatch(new Match(
+        cuartosDeFinalMundial.addMatch(new Match(
                 countries.get(2),
                 countries.get(3))
         );
-        octavosDeFinalMundial.addMatch(new Match(
+        cuartosDeFinalMundial.addMatch(new Match(
                 countries.get(4),
                 countries.get(5))
         );
-        octavosDeFinalMundial.addMatch(new Match(
+        cuartosDeFinalMundial.addMatch(new Match(
                 countries.get(6),
-                countries.get(7))
-        );
-        octavosDeFinalMundial.addMatch(new Match(
-                countries.get(8),
-                countries.get(9))
-        );
-        octavosDeFinalMundial.addMatch(new Match(
-                countries.get(10),
-                countries.get(11))
-        );
-        octavosDeFinalMundial.addMatch(new Match(
-                countries.get(12),
-                countries.get(13))
-        );
-        octavosDeFinalMundial.addMatch(new Match(
-                countries.get(14),
-                countries.get(15)
+                countries.get(7)
         ));
 
-        Match octavos1 = octavosDeFinalMundial.getMatches().get(0);
-        octavos1.incrementLocalGoals();
-        octavos1.incrementLocalGoals();
-        octavos1.incrementLocalGoals();
-        octavos1.incrementVisitantGoals();
+        Match cuartos1 = cuartosDeFinalMundial.getMatches().get(0);
+        cuartos1.incrementLocalGoals();
+        cuartos1.incrementVisitantGoals();
+        cuartos1.incrementVisitantGoals();
 
-        Match octavos2 = octavosDeFinalMundial.getMatches().get(1);
-        octavos2.incrementLocalGoals();
-        octavos2.incrementLocalGoals();
-        octavos2.incrementVisitantGoals();
+        Match cuartos2 = cuartosDeFinalMundial.getMatches().get(1);
+        cuartos2.incrementLocalGoals();
+        cuartos2.incrementVisitantGoals();
+        cuartos2.incrementVisitantGoals();
 
-        Match octavos3 = octavosDeFinalMundial.getMatches().get(2);
-        octavos3.incrementLocalGoals();
-        octavos3.incrementLocalGoals();
-        octavos3.incrementVisitantGoals();
+        Match cuartos3 = cuartosDeFinalMundial.getMatches().get(2);
+        cuartos3.incrementLocalGoals();
 
-        Match octavos4 = octavosDeFinalMundial.getMatches().get(3);
-        octavos4.incrementLocalGoals();
-        octavos4.incrementLocalGoals();
-        octavos4.incrementLocalGoals();
-        octavos4.incrementLocalGoals();
-        octavos4.incrementVisitantGoals();
+        Match cuartos4 = cuartosDeFinalMundial.getMatches().get(2);
+        cuartos4.incrementLocalGoals();
+        cuartos4.incrementLocalGoals();
+        cuartos3.incrementVisitantGoals();
 
-        Match octavos5 = octavosDeFinalMundial.getMatches().get(4);
-        octavos5.incrementVisitantGoals();
-        octavos5.incrementVisitantGoals();
-        octavos5.incrementVisitantGoals();
-
-        Match octavos6 = octavosDeFinalMundial.getMatches().get(5);
-        octavos6.incrementLocalGoals();
-        octavos6.incrementLocalGoals();
-        octavos6.incrementLocalGoals();
-        octavos6.incrementVisitantGoals();
-
-        Match octavos7 = octavosDeFinalMundial.getMatches().get(6);
-        octavos7.incrementLocalGoals();
-        octavos7.incrementLocalGoals();
-        octavos7.incrementVisitantGoals();
-
-        Match octavos8 = octavosDeFinalMundial.getMatches().get(7);
-        octavos8.incrementLocalGoals();
-        octavos8.incrementLocalGoals();
-        octavos8.incrementLocalGoals();
-        octavos8.incrementLocalGoals();
-        octavos8.incrementLocalGoals();
-        octavos8.incrementLocalGoals();
-        octavos8.incrementVisitantGoals();
-
-
-        octavosDeFinalMundial.getNextStepTeams().forEach(team -> {
+        cuartosDeFinalMundial.getNextStepTeams().forEach(team -> {
             team.showPlayersList();
         });
     }
